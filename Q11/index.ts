75a4f66714850ff9a07de24943791936c190004d
Hassan Naseer <139218360+Hassan-cyber-dot@users.noreply.github.com>
2023-10-13 22:30:52 -0700

    Create index.ts

---
+++ Q11/index.ts
@@ -0,0 +1,11 @@
+/* Store the names of a few of your friends in a array called names. Print each person’s 
+name by accessing each element in the list, one at a time.*/
+
+
+let friendsArray = ["rumman","bilal","talha"];
+
+console.log(friendsArray[0])
+console.log(friendsArray[1])
+console.log(friendsArray[2])
+
+
