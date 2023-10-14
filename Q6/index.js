688c9a3170df567f3f44ca4d626c89e463a69545
Hassan Naseer <139218360+Hassan-cyber-dot@users.noreply.github.com>
2023-10-13 22:14:31 -0700

    Create index.js

---
+++ Q6/index.js
@@ -0,0 +1,20 @@
+"use strict";
+/*Number Eight: Write addition, subtraction, multiplication, and division operations that
+ each result in the number 8. Be sure to enclose your operations in print statements
+  to see the results.*/
+//addition
+const num1 = 1;
+const num2 = 2;
+console.log(num1 + num2);
+//subtraction
+const a = 3;
+const b = 4;
+console.log(a - b);
+//multiplication
+const c = 5;
+const d = 6;
+console.log(c * d);
+//devision
+const e = 7;
+const f = 8;
+console.log(e / f);
